dcf3afd6a366adb73bf9552c295e189cfe3f4394
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2024-02-28 10:49:22 -0500

    renamed index.ts, moved listen to server.ts

--- app.ts
+++ app.ts
@@ -1,18 +1,13 @@
+import "dotenv/config";
 import express from "express";
 import { Client } from "@opensearch-project/opensearch";
-import * as dotenv from "dotenv";
-
-dotenv.config();
+import env from "./util/validateEnv";
 
 const app = express();
-const port = process.env.PORT;
 
 app.get('/', (req, res) =>
 {
     res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () =>
-{
-    console.log(`Server is running on port ${port}`);
-});
+export default app;
