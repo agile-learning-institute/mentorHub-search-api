70e4903630b8fe71afc1d97d6e8f6fa949b0487f
Andrew Klinsic <andrewklinsic@gmail.com>
2024-03-05 14:26:19 -0500

    +querybody class, ~fix search response

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import { OpensearchClient, Initialize } from "./client";
+import QueryBody from "./querybody";
 
 const app = express();
 
@@ -14,38 +15,19 @@ app.get('/', (req, res) =>
     res.send('Express + TypeScript Server');
 });
 
-interface queryBody
-{
-    query: {
-        prefix: {
-            [key: string]: string | number | undefined;
-        };
-    };
-    size: number;
-    from: number;
-}
-
 app.get('/api/search', async (req, res) =>
 {
     try {
         const queryString = req.headers.query as string;
         //structure the opensearch query
-        const queryBody: queryBody = {
-            query: {
-                prefix: {
-                    name: `${queryString}`,
-                }
-            },
-            size: 5,
-            from: 0,
-        };
+        const queryBody: QueryBody = new QueryBody(queryString, 5, 0);
         //actually use the query
         const searchRes = await client.search({
             index: "search-index",
             body: queryBody
         });
         //respond with opensearch response
-        res.status(200).json(searchRes);
+        res.status(200).json(searchRes.body);
     }
     catch (error) {
         console.error(error);
