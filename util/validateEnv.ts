92b8eb08f6ecb04db032be1bb275da3255fb6f20
Andrew Klinsic <andrewklinsic@gmail.com>
2024-03-18 21:12:05 -0400

    ~fix typos and botched refactor

--- util/validateEnv.ts
+++ util/validateEnv.ts
@@ -1,9 +1,10 @@
 import { cleanEnv, makeValidator } from "envalid";
 import { port, str } from "envalid";
 
-const connectionStringValidator = makeValidator((value: string | undefined) => {
+const connectionStringValidator = makeValidator((value: string | undefined) =>
+{
     if (value === undefined) {
-        return `${process.env.PROTOCOL}://${process.env.AUTH}@${process.env.HOST}:${process.env.OPENSEARCH_PORT}`;
+        return `${process.env.OPENSEARCH_PROTOCOL}://${process.env.OPENSEARCH_AUTH}@${process.env.OPENSEARCH_HOST}:${process.env.OPENSEARCH_PORT}`;
     }
     return value;
 });
