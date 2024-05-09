d5818fc74fe9af665c7abed6c5d28e8bb5d2034c
Andrew Klinsic <andrewklinsic@gmail.com>
2024-05-09 13:28:36 -0400

    fixed testsh

--- src/test.sh
+++ src/test.sh
@@ -4,7 +4,7 @@ if !([[ -d "./src/docker" ]] && [[ -d "./src/middleware" ]]); then
 fi
 
 mh down
-mh up elasticsearchonly
+mh up search-db
 #  sleep for 15 seconds; this is to ensure that the elasticsearch container is up and running before the script is executed, adjust as needed
 sleep 15
 
@@ -19,3 +19,4 @@ export API_PORT=8081
 export ELASTICSEARCH_PORT=9200
 export INDEX_NAME=search-index
 export CONNECTION_TIMEOUT=10
+npm run build && node ./dist/bundle.js
