6c654e503e5889632875826f98b30ca33de161f3
Andrew Klinsic <andrewklinsic@gmail.com>
2024-05-09 13:35:03 -0400

    added coverage report to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'], 
+  };
+  
