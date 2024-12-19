fd89c633b5091b50c76159f63e0ab052e4472327
Mike Storey <mike@agile-learning.institute>
2024-12-19 17:38:52 -0500

    env tests

--- src/config/Config.envConfig.test.ts
+++ src/config/Config.envConfig.test.ts
@@ -5,60 +5,92 @@ import config from './Config';
 
 describe('Config', () => {
 
-    test('test BUILT_AT', () => {
-        testConfigEnvironmentValue("BUILT_AT");
-    });
+    beforeEach(() => {
+        // Setup environment variables
+        for (const [key, defaultValue] of Object.entries({
+            ...config.stringPropertyDefaults,
+            ...config.secretStringPropertyDefaults
+        })) {
+            process.env[key] = "ENV_VALUE";
+        }
+        for (const [key, defaultValue] of Object.entries(
+            config.integerPropertyDefaults
+        )) {
+            process.env[key] = "1234";
+        }
+        for (const [key, defaultValue] of Object.entries(
+            config.secretJsonPropertyDefaults
+        )) {
+            process.env[key] = '{"foo":"bar"}';
+        }
 
-    test('test CONFIG_FOLDER', () => {
-        testConfigEnvironmentValue("CONFIG_FOLDER");
-    });
-
-    test('test API_PORT', () => {
-        testConfigEnvironmentValue("API_PORT");
-    });
-
-    test('test CONNECTION_STRING', () => {
-        testConfigEnvironmentValue("CONNECTION_STRING", true);
-    });
-
-    test('test PERSON_HOST', () => {
-        testConfigEnvironmentValue("PERSON_HOST");
-    });
-
-    test('test PARTNER_HOST', () => {
-        testConfigEnvironmentValue("PARTNER_HOST");
-    });
-
-    test('test TOPIC_HOST', () => {
-        testConfigEnvironmentValue("TOPIC_HOST");
-    });
-
-    test('test CURRICULUM_HOST', () => {
-        testConfigEnvironmentValue("CURRICULUM_HOST");
-    });
-
-    test('test ENCOUNTER_HOST', () => {
-        testConfigEnvironmentValue("ENCOUNTER_HOST");
-    });
-
-    // Test that a configuration value was properly loaded from an environment variable
-    function testConfigEnvironmentValue(configName: string, secret: boolean = false) {
-        process.env[configName] = '"ENVIRONMENT"';
+        // Initialize the Config
         config.initialize();
-        process.env[configName] = "";
 
+        // Housekeep Environment variables
+        process.env.CONFIG_FOLDER = "";
+        for (const [key, defaultValue] of Object.entries({
+            ...config.stringPropertyDefaults,
+            ...config.secretStringPropertyDefaults,
+            ...config.integerPropertyDefaults,
+            ...config.secretJsonPropertyDefaults
+        })) {
+            process.env[key] = "ENV_VALUE";
+        }
+    });
+
+    test('test envStrings', () => {
+        const testScope = {
+            ...config.stringPropertyDefaults,
+            ...config.secretStringPropertyDefaults
+        };
+        for (const [key, defaultValue] of Object.entries(testScope)) {
+            expect((config as any)[key]).toBe("ENV_VALUE");
+        }
+    });
+
+    test('test envIntegers', () => {
+        for (const [key, defaultValue] of Object.entries(config.integerPropertyDefaults)) {
+            expect((config as any)[key]).toBe(1234);
+        }
+    });
+
+    test('test envSecretJson', () => {
+        for (const [key, defaultValue] of Object.entries(config.secretJsonPropertyDefaults)) {
+            expect((config as any)[key]).toStrictEqual({"foo":"bar"});
+        }
+    });
+
+    test('test envConfigItemStringDefaults', () => {
+        for (const [key, defaultValue] of Object.entries(config.stringPropertyDefaults)) {
+            testConfigItemValue(key, "ENV_VALUE")
+        }
+    });
+
+    test('test envConfigItemIntegerDefaults', () => {
+        for (const [key, defaultValue] of Object.entries(config.integerPropertyDefaults)) {
+            testConfigItemValue(key, "1234")
+        }
+    });
+
+    test('test envConfigItemSecretDefaults', () => {
+        const testScope = {
+            ...config.secretJsonPropertyDefaults,
+            ...config.secretStringPropertyDefaults
+        }
+        for (const [key, defaultValue] of Object.entries(testScope)) {
+            testConfigItemValue(key, "secret")
+        }
+    });
+
+    function testConfigItemValue(configName: string, expectedValue: string) {
         const items = config.configItems;
-
         const item = items.find(i => i.name === configName);
         expect(item).toBeDefined();
         if (item) {
             expect(item.name).toBe(configName);
             expect(item.from).toBe("environment");
-            if (secret) {
-                expect(item.value).toBe("secret");
-            } else {
-                expect(item.value).toBe('"ENVIRONMENT"');
-            }
+            expect(item.value).toBe(expectedValue);
         }
     }
 
